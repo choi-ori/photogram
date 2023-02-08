0acb19628120e86651e888b6e1ea3e0806740a79
HyeonGyu Choi <chg0802@naver.com>
2023-02-08 15:47:34 +0900

    project commit

---
+++ photogram/src/main/java/com/cos/photogramstart/service/ImageService.java
@@ -0,0 +1,80 @@
+package com.cos.photogramstart.service;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import com.cos.photogramstart.domain.image.Image;
+import com.cos.photogramstart.domain.image.ImageRepository;
+import com.cos.photogramstart.web.dto.image.ImageUploadDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class ImageService {
+	
+	private final ImageRepository imageRepository;
+	
+	@Transactional(readOnly = true)
+	public List<Image> 인기사진(){
+		return imageRepository.mPopular();
+	}
+	
+	@Transactional(readOnly = true) // 영속성 컨텍스트 변경 감지를 해서, 더티체킹, flush(반영) X
+	public Page<Image> 이미지스토리(int principalId, Pageable pageable){
+		Page<Image> images = imageRepository.mStory(principalId, pageable);
+		
+		// 2(cos) 로그인
+		// images에 좋아요 상태 담기
+		images.forEach((image) -> {
+			
+			image.setLikeCount(image.getLikes().size());
+			
+			image.getLikes().forEach((like) -> {
+				if (like.getUser().getId() == principalId) { // 해당 이미지에 좋아요한 사람들을 찾아서 현재 로그인한 사람이 좋아요 한것인지 비교
+					image.setLikeState(true);
+				}
+			});
+			
+		});
+		
+		return images;
+	}
+	
+	@Value("${file.path}")
+	private String uploadFolder;
+	
+	@Transactional
+	public void 사진업로드(ImageUploadDto imageUploadDto, PrincipalDetails principalDetails) {
+		UUID uuid = UUID.randomUUID(); // uuid : 고유성이 보장되는 id를 만들기 위한 표준 규약
+		String imageFileName = uuid+"-"+imageUploadDto.getFile().getOriginalFilename(); // 파일명 중복되면 안되기 때문에 uuid 추가
+		System.out.println("이미지 파일이름 : " + imageFileName);
+		
+		Path imageFilePath = Paths.get(uploadFolder+imageFileName);
+		
+		// 통신, I/O -> 예외가 발생할 수 있다.
+		try {
+			Files.write(imageFilePath, imageUploadDto.getFile().getBytes());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// image 테이블에 저장
+		Image image = imageUploadDto.toEntity(imageFileName, principalDetails.getUser());
+		imageRepository.save(image);
+		
+		// 콘솔에 출력해보기 위해 작성
+//		Image imageEntity = imageRepository.save(image);
+//		System.out.println(imageEntity.toString());
+	}
+}
